6f5fe1334e512fb17b3c8419cd1a9d5292322fec
Alex Dedov <q.wwwww.q@inbox.ru>
2022-08-16 06:36:39 +0700

    Add seminar2

---
+++ Project2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void WriteSelect (string[] namesArray)
+    {
+        for (int i = 0; i < namesArray.Length-1; i++)
+        {
+            Console.Write(i+1 + " - " + namesArray[i] + ", ");
+        }
+        Console.WriteLine(namesArray.Length + " - " + namesArray[namesArray.Length-1] + ".");
+    }
+    static void SelectRase()
+    {
+        string[] namesRase = {"Люди", "Эльфы", "Гномы", "Орки"};
+        WriteSelect(namesRase);
+        Console.Write("Укажите вашу расу: ");
+        int indexRase = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ваша раса " + namesRase[indexRase-1]);
+    }
+    static void SelectClass()
+    {
+        string[] namesClass = {"Воин", "Маг", "Разбойник", "Вор"};
+        WriteSelect(namesClass);
+        Console.Write("Укажите вашу класс: ");
+        int indexClass = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ваш класс " + namesClass[indexClass-1]);
+    }
+    static void SelectPol()
+    {
+        string[] namesPol = {"Мужчина", "Женщина"};
+        WriteSelect(namesPol);
+        Console.Write("Укажите ваш пол: ");
+        int indexPol = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ваш пол " + namesPol[indexPol-1]);
+    }
+    static void Main (string [] args)
+    {
+        // int[] numbers = new int[4];
+        // int[] numb = {1,4,5,4};
+        SelectRase();
+        SelectClass();
+        SelectPol();
+    }
+}
